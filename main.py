6d8e3184f05499f3507e16f9a3b0c66f53a72068
VasantA2730 <vasantarul12@gmail.com>
2025-06-28 18:55:18 -0700

    Modified gitignore

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print('Hello Dashboard')
+import requests
+import json
+import pandas as pd
+
+data = requests.get("https://newsapi.org/v2/top-headlines?country=us&apiKey=ENV['AUTH_TOKEN']")
+
+articles = data.json()['articles']
+
+df = pd.DataFrame(articles)
+
+print(df.columns)
